f6f234b611b1ae6b5bf357a5a2b176c3df54196b
Mariley20 <mariley2018@gmail.com>
2017-11-26 21:02:26 -0500

    init 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import { connect } from "redux-zero/react";
+import {
+    HashRouter,
+    Redirect,
+    NavLink,
+    Route,
+    Switch,
+    BrowserRouter} from 'react-router-dom';
+// import './styles/App.css';
+// import {Home, Information, NavBar} from './Home';
+import {Login} from './Login';
+import {Portfolio} from './Portfolio';
+import {Habilities} from './Habilities';
+import {Contact} from './Contact';
+import {ViewWork} from './ViewWork';
+// import {Resume} from './Resume';
+const App = ({biografy, work, selected}) => {
+return (
+     <div className="container-fluid">
+        <HashRouter>
+            <Switch>
+                    <Route path="/login" render={() => <Login NavBar={NavBar} Information={Information}  />}/>
+                    {/* <Route path="/portfolio"  render={() => <Portfolio NavBar={NavBar} Information={Information} work={work} />}/>
+                    <Route path="/viewWork"  render={() => <ViewWork work={work} selected={selected} />}/>
+                    <Route path="/habilities"  render={() => <Habilities NavBar={NavBar} Information={Information} />}/>
+                    <Route path = "/contact" render = { () => <Contact NavBar={NavBar} Information={Information}/>} /> */}
+                    <Route path='/hack' render={() => <Redirect to="/login"/>}/>
+                    <Route exact path="/" render={() => <Login />}/>
+                    {/* <Route path="/resume"  render={() => <Resume biografy={biografy} selected={selected} />}/> */}
+            </Switch>
+        </HashRouter>
+    </div>
+)
 }
+const mapToProps = ({biografy, work, selected}) => ({biografy, work, selected});
 
-export default App;
+export default connect(mapToProps)(App);
